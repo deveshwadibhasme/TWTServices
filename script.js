19ef96fcd44df2e6af97d595c7e855bcc7d4ab77
deveshwadibhasme <146552994+deveshwadibhasme@users.noreply.github.com>
2024-08-20 14:04:28 +0530

    jijaji-twt

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ const menu = document.querySelector('.menu')
 const header = document.querySelector('header')
 
 menu.addEventListener('click', () =>{
-    menu.classList.toggle('active')
     header.classList.toggle('active')
 })
+
+menu.onClick.toggle(menu.innerHTML = '&Cross;')
+
 menu.addEventListener('touchup', () =>{
     menu.classList.toggle('active')
     header.classList.toggle('active')
