63f6d29f3650822379ecdd2d6744daf3c8e926d0
deveshwadibhasme <146552994+deveshwadibhasme@users.noreply.github.com>
2024-09-30 10:01:15 +0530

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+function loader() {
+    document.querySelector('#preloader').style.transform = 'translateY(-100%)'
+}
+function scrolling() {
+    document.body.style.overflow = 'visible'
+}
+window.addEventListener('load', () => {
+    setTimeout(loader, 2000)
+    setTimeout(scrolling,2500)
+})
+
 const menu = document.querySelector('.menu')
 const header = document.querySelector('header')
 
